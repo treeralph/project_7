42bd438f24cbba2f2e52c06741aae32f23502b66
treeralph <treeralph@gmail.com>
2024-05-13 10:36:37 +0900

    dev branch init

--- app/src/main/java/com/example/youtubeapi/network/RetrofitClient.kt
+++ app/src/main/java/com/example/youtubeapi/network/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = ""
+    private const val BASE_URL = "" // youtube api
     private val okHttpClient by lazy {
         OkHttpClient.Builder()
             .build()
